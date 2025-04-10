9bcc1e0d67cad05877176473b15a4f52f9012a6c
billykr67 <billykr67@gmail.com>
2025-04-10 02:42:14 +0000

    ini kedua

---
+++ lib/GaleriPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Galeripage extends StatefulWidget {
+  const Galeripage({super.key});
+
+  @override
+  State<Galeripage> createState() => _GaleripageState();
+}
+
+class _GaleripageState extends State<Galeripage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'GaleriPage.dart';
+
 void main() {
   runApp(
     MultiProvider(
@@ -35,8 +37,8 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
       body: [
-      // HomePage(),
-      // GaleriPage(),
+      HomePage(),
+      Galeripage(),
       // StorePage(),
       ][halsaatini],
       bottomNavigationBar: NavigationBar(
@@ -55,6 +57,54 @@ class _MyAppState extends State<MyApp> {
         ],
 
       ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) => ListTile(
+        leading: CircleAvatar(
+          foregroundImage: NetworkImage('https://picsum.photos/id/${index*5+2}/200'),
+          
+        ),
+          title:const Text('billy'),
+          subtitle: Text('Pinjam dulu ${index+1}00 besok diganti'),
+          trailing:const Text('12.0'),
+      ),
+    );
+  }
+}
+
+class TampilanListView extends StatelessWidget {
+  const TampilanListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        ListTile(
+          leading: CircleAvatar(
+            foregroundImage: NetworkImage('https://picsum.photos/id/237/200'),
+          ),
+          title:const Text('billy'),
+          subtitle: Text('Pinjam dulu besok diganti'),
+
+        ),
+          ListTile(
+          leading: CircleAvatar(
+            foregroundImage: NetworkImage('https://picsum.photos/id/237/200'),
+          ),
+          title:const Text('billy'),
+          subtitle: Text('Pinjam dulu besok diganti'),
+
+        ),
+      ],
     );
   }
 }

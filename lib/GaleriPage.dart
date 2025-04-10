12d969833ad8d03c52ad50ae071083d0aca0cdba
billykr67 <billykr67@gmail.com>
2025-04-10 02:45:39 +0000

    kedua revisi

--- lib/GaleriPage.dart
+++ lib/GaleriPage.dart
@@ -8,10 +8,35 @@ class Galeripage extends StatefulWidget {
 }
 
 class _GaleripageState extends State<Galeripage> {
+  final List listFavorit = List.generate(10, (index) => false);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5.5),
+        child: GridView.builder(
+          itemCount: 12,
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            mainAxisSpacing: 5.5,
+            crossAxisSpacing: 5.5,
+          ),
+          itemBuilder: (context, index) {
+            return GridTile(
+              footer: Container(
+                color: Colors.black.withOpacity(0.5),
+                alignment: Alignment.centerRight,
+                child: IconButton(onPressed: (){
+                    setState(() {
+                      listFavorit[index] = !listFavorit[index];
+                    });
+                },
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
